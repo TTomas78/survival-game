c131f57118171b21255e185b06911b2a114f5c22
gian <gianfranco.jsx@gmail.com>
2022-09-08 14:54:42 -0300

    spawn wood

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    [SerializeField] GameObject woodPrefab;
+    [SerializeField] float spawnRadius = 2;
+    [SerializeField] int health = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // get the mouse position
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // check if the mouse position is inside the collider
+            if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(mousePos))
+            {
+                Debug.Log("Clicked on the tree");
+                if (health >= 1)
+                    StartCoroutine(SpawnWoodRoutine());
+                if (health < 1)
+                    Destroy(gameObject);
+            }
+        }
+    }
+
+    IEnumerator SpawnWoodRoutine()
+    {
+        {
+            yield return new WaitForSeconds(0.1f);
+            Vector2 randomPosition = Random.insideUnitCircle * spawnRadius;
+            Instantiate(woodPrefab, randomPosition, Quaternion.identity);
+            health--;
+        }
+    }
+}
